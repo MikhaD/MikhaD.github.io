51775dcbbc4409791eb39d50d13b417eea143bce
MikhaD <mikhadavids@gmail.com>
2023-05-18 16:53:56 +0200

    feat: :sparkles: basic landing page

---
+++ src/lib/Glitch.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	export let text: string;
+	export let iteration_factor = 2;
+	export let flash_duration = 30;
+	$: display = text;
+	const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+	let interval: number | undefined;
+
+	function onMouseover() {
+		let iterations = 0;
+		clearInterval(interval);
+		interval = setInterval(() => {
+			let result = "";
+			for (let i = 0; i < text.length; ++i) {
+				if (i * iteration_factor < iterations) {
+					result += text[i];
+				} else {
+					let letter = ALPHABET[Math.floor(Math.random() * ALPHABET.length)];
+					if (Math.random() > 0.5) {
+						letter = letter.toUpperCase();
+					}
+					result += letter;
+				}
+			}
+			display = result;
+			if (iterations == text.length * iteration_factor) {
+				clearInterval(interval);
+			}
+			iterations += 1;
+		}, flash_duration);
+	}
+</script>
+
+<span on:mouseover={onMouseover} on:focus={onMouseover}>{display}</span>
+
+<style>
+	span {
+		font-family: monospace;
+		user-select: none;
+
+		display: inline-block;
+		color: white;
+		padding: 0.5rem 1rem;
+		font-size: 2em;
+	}
+</style>
